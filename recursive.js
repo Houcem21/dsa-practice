bf23344b1d96cf3674da8d8047190f0cfb529d27
Sam <damakhoucem90@gmail.com>
2025-03-19 18:51:35 +0100

    end-recursion

--- recursive.js
+++ recursive.js
@@ -48,4 +48,66 @@ function permutate(array, pointer=array.length, permutations=[]) {
     return permutate(array, pointer-1, permutations)
 }
 
-console.log(permutate(['F','A','C','E']))
+function swap(strArr, ind1, ind2) {
+    let temp = strArr[ind1]
+    strArr[ind1] = strArr[ind2]
+    strArr[ind2] = temp;
+}
+
+function permute(strArr, begin, end) {
+    if (begin == end) {
+        console.log(strArr)
+    } else {
+        for (let i = begin; i < end + 1; i++) {
+            swap(strArr, begin, i)
+            permute(strArr, begin + 1, end)
+            swap(strArr, begin, i)
+        }
+    }
+}
+
+function permuteArray(strArr) {
+    permute(strArr, 0, strArr.length -1)
+}
+
+// Flatten dictionary
+let dictionary = {
+    'Key1': '0',
+    'Key2': {
+        'a': '2',
+        'b': '3',
+        'c': {
+            'd': '3',
+            'e': '5'
+        }
+    }
+}
+
+function flatten(dictionary, flattened = {}) {
+    for (var item in dictionary) {
+        const element = dictionary[item]
+        if(typeof(element) !== "object") {
+            flattened[item] = element
+        }
+        else flatten(element, flattened)
+    }
+    return flattened
+}
+
+// Palindrome Checker
+const pal = "aibohphobia"
+
+function palindromeCheck(str, index= (str.length % 2 === 0) ? .5 : 0) {
+    const mid = str.length / 2 - .5
+    
+    const upperInd = Math.floor(mid-index)
+    const lowerInd = Math.floor(mid+index)
+    console.log(upperInd,mid, lowerInd, index)
+    if (index >= mid) return true
+    if (str[upperInd] !== str[lowerInd]) return false
+    index++
+    return palindromeCheck(str, index)
+}
+
+console.log(palindromeCheck(pal))
+console.log(palindromeCheck('egfillifge'))
