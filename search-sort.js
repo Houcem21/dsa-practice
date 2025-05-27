24e56640451a1ac78fe748678d3408cadccd046c
Sam <damakhoucem90@gmail.com>
2025-05-27 18:56:22 +0100

    insertion-done-quicksort-half

--- search-sort.js
+++ search-sort.js
@@ -1,3 +1,5 @@
+import { display } from "./base.js";
+
 // Linear Search
 function linearSearch(arr, query) {
     for (let i = 0; i < arr.length; i++) {
@@ -64,7 +66,6 @@ function binaryFreshSearch(array, q, begIndex = 0, endIndex = array.length) {
     return binaryFreshSearch(array.slice(begIndex, endIndex),q)
 }
 
-
 // Bubble sort
 function bubbleSort(array) {
     for (let i = 0; i < array.length-1; i++) {
@@ -75,18 +76,93 @@ function bubbleSort(array) {
     console.log(array)
 }
 
+function findMin(array) {
+    let min = {'value': array[0], 'index': 0}
+    for (let i = 1; i < array.length; i++) {
+        const element = array[i];
+        if (element < min.value) {
+            min.value = element;
+            min.index = i
+        }
+    }
+    return min;
+}
 
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
+        let element = arr[i];
         const remArray = arr.slice(i)
-        const minEl = Math.min(...remArray)
-        console.log(remArray,minEl)
-        let temp = element;
+        const minEl = findMin(remArray)
+        console.log(arr,minEl.value, minEl.index+i)
+        arr[i] = minEl.value
+        arr[minEl.index+i] = element
     }
 }
 
-const array = [11,0,3,14,1,6,14,17,4,2,3]
-const result = selectionSort(array)
+function insertionSort(arr) {
+    let i,j,len=arr.length,value
 
-console.log(result)
+    for (i = 0; i < len; i++) {
+        value  = arr[i];
+        console.log(value)
+        for (j = i-1; j > -1 && arr[j]>value; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = value
+    }
+    return arr
+}
+
+function swapItems(arr,i,j) {
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+function findBigger(arr,val) {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element>val) return {'index':i,'value':element}
+    }
+    return null
+}
+
+function findSmaller(arr,val) {
+    for (let j = arr.length; j > -1; j--) {
+        const element = arr[j];
+        if (element<val) return {'index':j,'value':element}
+    }
+    return null
+}
+
+function subArray(arr, begIndex=0, endIndex=arr.length) {
+    return arr.slice(begIndex,endIndex)
+}
+
+function quickSort(arr) {
+    let len=arr.length,i,j,pivot,median, biggerFromLeft, smallerFromRight
+    median = Math.floor(len/2)
+    pivot = arr[median]
+    swapItems(arr,median,len-1)
+    while (true) {
+        biggerFromLeft = findBigger(arr,pivot)
+        smallerFromRight = findSmaller(arr,pivot)
+        console.log(len, median,pivot,biggerFromLeft,smallerFromRight)
+        if (!biggerFromLeft || !smallerFromRight) {
+            swapItems(arr,biggerFromLeft.index,len-1)
+            break;
+        }
+        if(biggerFromLeft?.index > smallerFromRight?.index) {
+            swapItems(arr,biggerFromLeft.index,len-1)
+            break
+        }
+        swapItems(arr,biggerFromLeft.index,smallerFromRight.index)
+    }
+    console.log(subArray(arr,0,median+1),subArray(arr,median+2))
+    return arr
+}
+
+let array = [11,0,3,14,12,6,14,7,4,2,3,4,11,9,1,3]
+const result = quickSort(array)
+
+display(result)
