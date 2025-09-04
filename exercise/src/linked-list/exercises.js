2cdcadac7f448f840638659aedd402fcf9a7072c
Sam <damakhoucem90@gmail.com>
2025-09-04 16:37:17 +0900

    p203

--- exercise/src/linked-list/exercises.js
+++ exercise/src/linked-list/exercises.js
@@ -3,18 +3,43 @@
 import {SinglyLinkedList} from './singleLinked.js';
 
 function reverseSinglyLinkedList(sll) {
+    let reversedList = new SinglyLinkedList();
+    const firstHead = sll.head;
     for (let ct= 0; ct < sll.size; ct++) {
         console.log(sll.head)
-        
+        reversedList.insert(sll.head);
         sll.head = sll.head.next
-
     }
+    sll.head = firstHead;
+    return reversedList;
 }
 
+function deleteDuplicates(sll) {
+    let uniqueValues = new Set();
+    let currentNode = sll.head;
+    let prevNode = null;
+    const originalSize = sll.size;
+    for (let ct= 0; ct < originalSize; ct++) {
+        if( uniqueValues.has(currentNode.data)) {
+            prevNode.next = currentNode.next;
+            currentNode = prevNode;
+            sll.size--;
+        }
+        uniqueValues.add(currentNode.data)
+        prevNode = currentNode
+        currentNode = currentNode.next
+    }
+    console.log(sll)
+    return uniqueValues;
+}
 
 const sll = new SinglyLinkedList();
 sll.insert('a')
 sll.insert('b')
 sll.insert('c')
 sll.insert('d')
-reverseSinglyLinkedList(sll)
+sll.insert('d')
+sll.insert('b')
+sll.insert('c')
+sll.insert('e')
+console.log(deleteDuplicates(sll))
