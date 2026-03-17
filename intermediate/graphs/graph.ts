36fa53533e545995d41d0bdc79830e4255954e05
Sam <damakhoucem90@gmail.com>
2026-03-17 23:06:15 +0300

    cypress-and-graph-287

--- intermediate/graphs/graph.ts
+++ intermediate/graphs/graph.ts
@@ -1,30 +1,51 @@
-interface weightedEdge {
+interface edge {
+    head: number;
     target: number;
-    weight: number;
+    weight?: number;
 }
 
 class UndirectedGraph {
-    private edgeValue: Set<weightedEdge> = new Set<weightedEdge>;
-    private edges: Map<number, Set<weightedEdge>>;
-    
+    private graph : Map<number, Set<edge>>;
+
     constructor() {
-        this.edges = new Map();
+        this.graph = new Map();
     }
 
-    public addVertex(vertex: number) {
-        this.edges.set(vertex, this.edgeValue);
+    public addVertex(value: number): void {
+        if (this.graph.has(value)) return
+        const newEdgesList: Set<edge> = new Set();
+        this.graph.set(value, newEdgesList);
     }
-    public addEdge(vertex1: number, vertex2: number, weight: number) {
-        if(!weight) weight = 0;
-        const edVal: weightedEdge = {
-            target: vertex2,
-            weight: weight
+
+    public addEdge(edge: edge): void {
+        if (!this.graph.has(edge.head)) this.addVertex(edge.head);
+        if (!this.graph.has(edge.target)) this.addVertex(edge.target);
+
+        if (this.graph.has(edge.head) && this.graph.has(edge.target)) {
+            for (let e of this.graph.get(edge.head)!) {
+                if ((e.head == edge.head && e.target == edge.target) || (e.head == edge.target && e.target == edge.head)) return;
+            }
         }
-        const newVal : Set<weightedEdge> = 
-        this.edges.get(vertex1)?.add(edVal) || new Set<weightedEdge>;
-        this.edges.set(vertex1, newVal);
+        // Get old lists
+        const oldHeadEdgesList = this.graph.get(edge.head)!;
+        const oldTargetEdgesList = this.graph.get(edge.target)!;
+
+        // Update both vertices with edge
+        this.graph.set(edge.head, oldHeadEdgesList.add(edge));
+        this.graph.set(edge.target, oldTargetEdgesList.add(edge));
+    }
+
+    public display() {
+        console.log(
+            Object.fromEntries(
+                Array.from(this.graph.entries(), ([key, set]) => [key, Array.from(set)])
+            )
+        );
     }
 }
 
-const uGraph = new UndirectedGraph();
-uGraph.addVertex(1);
+const undirectedGraph = new UndirectedGraph();
+undirectedGraph.addEdge({ head: 1, target: 2, weight: 5 });
+undirectedGraph.addEdge({ head: 1, target: 2, weight: 5 });
+
+undirectedGraph.display();
